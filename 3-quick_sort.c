4314a7b0bfccfd9e0ee33f4c78657edcb56783be
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-11-07 19:46:38 +0300

    Betty Improved

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,66 +1,69 @@
 #include "sort.h"
 
 /**
-* swap_values - Swaps values from two different array indexes
-* @array:       Array to swap values from.
+* swap_values -	Swaps values from two different array indexes
+* @array:       Array to swap values from
 * @i:           value under index (i) to swap
 * @j:           value under index (j) to swap
 * @size:        size of the array
 */
 void swap_values(int *array, int i, int j, int size)
 {
-    int copy_num = 0;
+	int copy_num = 0;
 
-    copy_num = array[i];
-    array[i] = array[j];
-    array[j] = copy_num;
-    print_array(array, size);
+	copy_num = array[i];
+	array[i] = array[j];
+	array[j] = copy_num;
+	print_array(array, size);
 }
 
 /**
 * lomuto_partition -    Partitions array into two parts based on pivot
-* @array:               Array to partition.
+* @array:				Array to partition.
 * @low:                 value in the lowest index
 * @high:                value in the highest index
 * @size:                size of the array
-* @Return:              Returns two partitions based on partition value
+* Return:              Returns two partitions based on partition value
 */
 int lomuto_partition(int *array, int low, int high, int size)
 {
-    int i = 0, j = 0, pivot = 0;
-    pivot = array[high];
-    i = low;
+	int i = 0, j = 0, pivot = 0;
 
-    for (j = low; j < high; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            if (j != i)
-                swap_values(array, j, i, size);
-            i++;
-        }
-    }
-    if (i != high)
-        swap_values(array, i, high, size);
-    return (i);
+	pivot = array[high];
+
+	i = low;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			if (j != i)
+				swap_values(array, j, i, size);
+			i++;
+		}
+	}
+	if (i != high)
+		swap_values(array, i, high, size);
+	return (i);
 }
 
 /**
 * recursive_partition - Helper function for Quick Sort
+* @array:				Array to be partitioned
 * @low:                 index to low element
 * @high:                index to high element
 * @size:                size of the array
 */
 void recursive_partition(int *array, int low, int high, int size)
 {
-    int pivot = 0;
+	int pivot = 0;
 
-    if (low < high)
-    {
-        pivot = lomuto_partition(array, low, high, size);
-        recursive_partition(array, low, pivot - 1, size);
-        recursive_partition(array, pivot + 1, high, size);
-    }
+	if (low < high)
+	{
+		pivot = lomuto_partition(array, low, high, size);
+		recursive_partition(array, low, pivot - 1, size);
+		recursive_partition(array, pivot + 1, high, size);
+	}
 }
 
 
@@ -72,8 +75,9 @@ void recursive_partition(int *array, int low, int high, int size)
 */
 void quick_sort(int *array, size_t size)
 {
-    size_t low = 0, high = 0;
-    high = (size - 1);
+	size_t low = 0, high = 0;
 
-    recursive_partition(array, low, high, size);
+	high = (size - 1);
+
+	recursive_partition(array, low, high, size);
 }
